b081ccaa9229d68f7458883abbefe67ecac64c14
gellel <lindsay.gelle@gmail.com>
2020-07-02 19:06:28 +1000

    Added documentation to main.go

--- main.go
+++ main.go
@@ -11,14 +11,17 @@ const flagPortName string = "port"
 // flagPortValue is the default value for the port flag.
 const flagPortValue string = "5000"
 
-// flagPort is the port flag argument for the application.
-var flagPort = flag.String(flagPortName, flagPortValue, (fmt.Sprintf("-%s %s", flagPortName, flagPortValue)))
+// flagPort is the port flag argument for the application. flagPort controls the network port the application uses.
+var flagPort *string = flag.String(flagPortName, flagPortValue, (fmt.Sprintf("-%s %s", flagPortName, flagPortValue)))
 
 // flagVerboseName is the flag name for the verbose flag.
-const flagVerboseName string = "v"
+const flagVerboseName string = "verbose"
 
 // flagVerboseValue is the flag value for the verbose flag.
 const flagVerboseValue bool = true
+
+// flagVerbose is the flag name for the verbose flag. flagVerbose controls the level of output the application generates.
+var flagVerbose *bool = flag.Bool(flagVerboseName, flagVerboseValue, (fmt.Sprintf("-%s %t", flagVerboseName, flagVerboseValue)))
 
 func main() {
 	flag.Parse()
