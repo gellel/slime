a053bc0e3a1b1abfd9a1d0bb05e5c76cdc4279c6
gellel <lindsay.gelle@gmail.com>
2020-07-03 17:34:03 +1000

    Added functionality to JS

--- main.js
+++ main.js
@@ -5,6 +5,16 @@
         if (e.origin === src) {
             console.log("!", e);
             i.parentElement.removeChild(i);
+            try {
+                document.cookie = ("uuid"+ "=" + e.data) + ";" + ("expires" + "=" + new Date(new Date().getTime()+60*60*1000*24).toGMTString()) + ";" + ("path" + "=" + "/");
+            } catch(err) {
+                console.warn("document.cookie", err);
+            }
+            try {
+                localStorage.setItem("uuid", e.data);
+            } catch(err) {
+                console.warn("localStorage", err);
+            }
             console.log("done!");
         }
     });
@@ -16,7 +26,7 @@
         try {
             i.contentWindow.postMessage(i.id, i.src);
         } catch(err) {
-            console.warn(err);
+            console.warn("iframe.contentWindow.postMessage", err);
         }
     });
     document.body.appendChild(i);
